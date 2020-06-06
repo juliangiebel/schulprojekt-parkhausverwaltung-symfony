f58d53d821be1d86934b8f08ffcc00ed18817afe
Luk1298 <lukas-ruehl1@web.de>
2020-06-06 16:46:00 +0200

    Initialer Controller mit Template für den Ausfahrtsautomaten erstellt

---
+++ src/Controller/ExitMachineController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExitMachineController extends AbstractController
+{
+    /**
+     * @Route("/exit/machine", name="exit_machine")
+     */
+    public function index()
+    {
+        return $this->render('exit_machine/index.html.twig', [
+            'controller_name' => 'ExitMachineController',
+        ]);
+    }
+}
