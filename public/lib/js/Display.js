c7fe33dff82572257b26ae0371fe1a0dd0a3ccff
Luk1298 <lukas-ruehl1@web.de>
2020-06-18 19:06:02 +0200

    * Module für das Kennzeichen und das Display angepasst * Tabs in Leerzeichen umgewandelt * Kennzeichen-Funktion "isLegal" angepasst bzw. Fehler behoben

--- public/lib/js/Display.js
+++ public/lib/js/Display.js
@@ -1,54 +1,70 @@
 class Display {
 
-	constructor(objectId) {
-		this.objectDisplay = $(objectId);
-	}
+    constructor(objectId)
+    {
+        this.objectDisplay = $(objectId);
+    }
 
-	updateDate()
-	{
-		let date     = new Date();
-		let shortDay = {
-			0: 'So',
-			1: 'Mo',
-			2: 'Di',
-			3: 'Mi',
-			4: 'Do',
-			5: 'Fr',
-			6: 'Sa',
-		}
-		[date.getDay()];
+    updateDate()
+    {
+        let date     = new Date();
+        let shortDay = {
+            0: 'So',
+            1: 'Mo',
+            2: 'Di',
+            3: 'Mi',
+            4: 'Do',
+            5: 'Fr',
+            6: 'Sa',
+        }
+        [date.getDay()];
 
-		this.objectDisplay.find('div#display-date').text(shortDay + ' ' +
-			(date.getDate()      <= 9 ? '0' +  date.getDate()       :  date.getDate()      ) + '.' +
-			(date.getMonth() + 1 <= 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) + '.' +
-			 date.getFullYear()		
-		);
-	}
+        this.objectDisplay.find('div#display-date').text(shortDay + ' ' +
+            (date.getDate()      <= 9 ? '0' +  date.getDate()       :  date.getDate()      ) + '.' +
+            (date.getMonth() + 1 <= 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) + '.' +
+             date.getFullYear()
+        );
+    }
 
-	updateTime()
-	{
-		var time = new Date();
+    updateTime()
+    {
+        var time = new Date();
 
-		this.objectDisplay.find('div#display-time').text(
-			(time.getHours()   <= 9 ? '0' + time.getHours()   : time.getHours()  ) + ':' +
-			(time.getMinutes() <= 9 ? '0' + time.getMinutes() : time.getMinutes()) + ':' +
-			(time.getSeconds() <= 9 ? '0' + time.getSeconds() : time.getSeconds())
-		);
-	}
+        this.objectDisplay.find('div#display-time').text(
+            (time.getHours()   <= 9 ? '0' + time.getHours()   : time.getHours()  ) + ':' +
+            (time.getMinutes() <= 9 ? '0' + time.getMinutes() : time.getMinutes()) + ':' +
+            (time.getSeconds() <= 9 ? '0' + time.getSeconds() : time.getSeconds())
+        );
+    }
 
-	setLtpState(isActive) {
-		this.objectDisplay.find('#display-is-ltp').text(isActive ? 'x' : ' ');
-	}
+    setMessage(msg, style)
+    {
+        this.objectDisplay.find('div#display-message').text(msg);
 
-	setPlate(plate) {
-		this.objectDisplay.find('#display-plate').text(plate.toString());
+        if (style !== undefined) {
+            this.objectDisplay.find('div#display-message').css(style);
+        }
+    }
 
-		if (!plate.isValid()) {
-			this.objectDisplay.find('#display-plate').css({color: 'red'});
-		}
-		else {
-			this.objectDisplay.find('#display-plate').css({color: 'navajowhite'});
-		}
-	}
+    resetMessage()
+    {
+        this.objectDisplay.find('div#display-message').html('&nbsp;').css({width: '100%'});
+    }
 
+    setLtpState(isActive)
+    {
+        this.objectDisplay.find('#display-is-ltp').text(isActive ? 'x' : ' ');
+    }
+
+    setPlate(plate)
+    {
+        this.objectDisplay.find('#display-plate').text(plate.toString());
+
+        if (!plate.isValid()) {
+            this.objectDisplay.find('#display-plate').css({color: 'red'});
+        }
+        else {
+            this.objectDisplay.find('#display-plate').css({color: 'navajowhite'});
+        }
+    }
 }
--- public/lib/js/LicensePlate.js
+++ public/lib/js/LicensePlate.js
@@ -1,59 +1,68 @@
 class LicensePlate {
 
-	_partFor = {
-		district: null,
-		custom:   null,
-		number:   null,
-		addition: null,
-	}
+    _partFor = {
+        district: null,
+        custom:   null,
+        number:   null,
+        addition: null,
+    }
 
-	_regexPartFor = {
-		district: /^[a-zA-Z]{1,3}$/,
-		custom:   /^[a-zA-Z]{1,2}$/,
-		number:   /^[0-9]{1,4}$/,
-		addition: /^(E|H)?$/,
-	}
+    _regexPartFor = {
+        district: /^[a-z]{1,3}$/i,
+        custom:   /^[a-z]{1,2}$/i,
+        number:   /^[0-9]{1,4}$/,
+        addition: /^(e|h)?$/i,
+    }
 
-	constructor()
-	{
+    _illegalRegexPartFor = {
+        custom: /^(kz|hj|ns|sa|ss)$/i,
+    };
 
-	}
+    constructor()
+    {
 
-	set(data)
-	{
-		this._partFor.district = data.district !== undefined ? data.district : this._partFor.district;
-		this._partFor.custom   = data.custom   !== undefined ? data.custom   : this._partFor.custom;
-		this._partFor.number   = data.number   !== undefined ? data.number   : this._partFor.number;
-		this._partFor.addition = data.addition !== undefined ? data.addition : this._partFor.addition;
-	}
+    }
 
-	isValid()
-	{
-		return (
-			(
-				   this._partFor.district !== null
-				&& this._partFor.district.match(this._regexPartFor.district)
-			) && (
-				   this._partFor.custom !== null
-				&& this._partFor.custom.match(this._regexPartFor.custom)
-			) && (
-				   this._partFor.number !== null
-				&& this._partFor.number.match(this._regexPartFor.number)
-			) && (
-				   this._partFor.addition === null
-				|| this._partFor.addition.match(this._regexPartFor.addition)
-			)
-		);
-	}
+    set(data)
+    {
+        this._partFor.district = data.district !== undefined ? data.district : this._partFor.district;
+        this._partFor.custom   = data.custom   !== undefined ? data.custom   : this._partFor.custom;
+        this._partFor.number   = data.number   !== undefined ? data.number   : this._partFor.number;
+        this._partFor.addition = data.addition !== undefined ? data.addition : this._partFor.addition;
+    }
 
-	toString()
-	{
-		return '' +
-			(this._partFor.district !== null && this._partFor.district.length >= 1 ? this._partFor.district : '---') + ' ' +
-			(this._partFor.custom   !== null && this._partFor.custom  .length >= 1 ? this._partFor.custom   : '---') + ' ' +
-			(this._partFor.number   !== null && this._partFor.number  .length >= 1 ? this._partFor.number   : '---') + ' ' +
-			(this._partFor.addition !== null && this._partFor.addition.length >= 1 ? this._partFor.addition : ''   )
-		;
-	}
+    isValid()
+    {
+        return (
+            (
+                   this._partFor.district !== null
+                && this._partFor.district.match(this._regexPartFor.district)
+            ) && (
+                   this._partFor.custom !== null
+                && this._partFor.custom.match(this._regexPartFor.custom)
+            ) && (
+                   this._partFor.number !== null
+                && this._partFor.number.match(this._regexPartFor.number)
+            ) && (
+                   this._partFor.addition === null
+                || this._partFor.addition.match(this._regexPartFor.addition)
+            )
+        );
+    }
+
+    isLegal()
+    {
+        return this._partFor.custom === null || this._illegalRegexPartFor.custom === null || !this._partFor.custom.match(this._illegalRegexPartFor.custom);
+    }
+
+    toString()
+    {
+        return '' +
+            (this._partFor.district !== null && this._partFor.district.length >= 1 ? this._partFor.district : '---') + ' ' +
+            (this._partFor.custom   !== null && this._partFor.custom  .length >= 1 ? this._partFor.custom   : '---') + ' ' +
+            (this._partFor.number   !== null && this._partFor.number  .length >= 1 ? this._partFor.number   : '---') + ' ' +
+            (this._partFor.addition !== null && this._partFor.addition.length >= 1 ? this._partFor.addition : ''   )
+        ;
+    }
 
 }
